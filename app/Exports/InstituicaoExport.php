6dae7cb6fce1d0416c44e9a43192f618aa5d54a9
callcocam <contato@sigasmart.com.br>
2022-07-05 11:20:35 -0300

    relatorios

--- app/Exports/InstituicaoExport.php
+++ app/Exports/InstituicaoExport.php
@@ -15,18 +15,21 @@ class InstituicaoExport implements FromCollection
         $addresses = \App\Models\Address::query()->where('addressable_type',\App\Models\Instituicao::class)->whereNotNull('city')->orderBy('city')->get();
         // BLZ AGORA EU QUERO IMPRIMIR UMA LISTA DE TODOS RPPS POR CIDADE COM NOME DO PRESIDENTE TELEFONE E EMAIL
         $data = [];
-        foreach ($addresses as $key => $value) {
-            $instituto = \App\Models\Instituicao::query()->find(data_get($value,'addressable_id'));
-            $representante = $instituto->representante;
-            $data[] = [
-                data_get($instituto, "name"),
-                data_get($representante, "name"),
-                data_get($representante, "email"),
-                data_get($instituto, "email"),
-                data_get($representante, "phone"),
-                data_get($value, "city"),
-                data_get($value, "state"),
-            ];
+        if($instituicao_tipo = \App\Models\InstituicaoTipo::query()->where("slug",config('instituicao.tipo.slug.acossiado','associados'))->first()){
+            foreach ($addresses as $key => $value) {
+                if($instituto = \App\Models\Instituicao::query()->where(["instituicao_tipo_id"=> $instituicao_tipo->id, 'id'=>data_get($value,'addressable_id')])->first()){
+                    $representante = $instituto->representante;
+                    $data[] = [
+                        data_get($instituto, "name"),
+                        data_get($representante, "name"),
+                        data_get($representante, "email"),
+                        data_get($instituto, "email"),
+                        data_get($representante, "phone"),
+                        data_get($value, "city"),
+                        data_get($value, "state"),
+                    ];
+                }
+            }
         }
        return collect($data);
     }
